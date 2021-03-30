0131ead1e5fd2b10deab14a6340caf3728b57967
JC <bonassin@gmail.com>
2021-03-30 15:21:50 +0000

    Automatically backed up by Learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,9 +1,12 @@
 class OwnersController < ApplicationController
 
-  get '/owners' do
-    @owners = Owner.all
-    erb :'/owners/index'
+  post '/owners' do
+  @owner = Owner.create(params[:owner])
+  if !params["pet"]["name"].empty?
+    @owner.pets << Pet.create(name: params["pet"]["name"])
   end
+  redirect "owners/#{@owner.id}"
+end
 
   get '/owners/new' do
     @pets = Pet.all
